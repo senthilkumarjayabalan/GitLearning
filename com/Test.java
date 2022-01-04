1a695bcfaf095e02b698538470cf461241b498e6
senthilkumarjayabalan <senthil.jaybalan@gmail.com>
2022-01-04 22:11:22 +0530

    From local

--- com/Test.java
+++ com/Test.java
@@ -6,6 +6,7 @@ class Test
 public static void main(String[] args) {
 	System.out.println("Git Learning");
 	System.out.println("Git Learning 2");
+	System.out.println("File Modified in Local");
 
 }
 
